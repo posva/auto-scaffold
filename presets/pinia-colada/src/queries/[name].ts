4f19dfc8a3f006880d75c03f0052dc1cd751a149
Eduardo San Martin Morote <posva13@gmail.com>
2026-01-14 17:04:06 +0100

    feat: improve presets

--- presets/pinia-colada/src/queries/[name].ts
+++ presets/pinia-colada/src/queries/[name].ts
@@ -1,8 +1,11 @@
-import { defineQuery } from '@pinia/colada'
+import { defineQueryOptions } from '@pinia/colada'
 
-export const useQuery = defineQuery({
-  key: [''],
-  query: async () => {
-    // TODO: implement
-  },
-})
+export const QUERY_KEYS = {
+  root: ['root'] as const,
+  byId: (id: string) => [...QUERY_KEYS.root, id] as const,
+}
+
+export const byIdQuery = defineQueryOptions(({ id }: { id: string }) => ({
+  key: QUERY_KEYS.byId(id),
+  query: () => Promise.reject('TODO:'),
+}))
