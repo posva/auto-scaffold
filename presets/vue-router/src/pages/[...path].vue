6b7c121f8ceff099d67abfa9c7421416f70cf597
Eduardo San Martin Morote <posva13@gmail.com>
2026-01-14 17:04:06 +0100

    feat: improve presets

--- presets/vue-router/src/pages/[...path].vue
+++ presets/vue-router/src/pages/[...path].vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+</script>
 
 <template>
-  <div></div>
+  <h2>{{ route.path }}</h2>
 </template>
